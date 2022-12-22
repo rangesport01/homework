f539f125292e2e929ad4af6d50ddddd2ec8c515a
rangesport01 <118473454+rangesport01@users.noreply.github.com>
2022-12-22 15:38:13 +0300

    without a robot

--- lesson.py
+++ lesson.py
@@ -2,3 +2,12 @@ def find_it(seq):
     result = list(filter(lambda x: seq.count(x) % 2 != 0, seq))
     return result[0]
 print(find_it([1,1,1,2,2,3,1,1,1]))
+
+from math import sqrt
+
+def is_square(n):
+    if sqrt(n) == int(sqrt(n)) :
+        return True
+    else:
+        return False
+print(is_square(25))
